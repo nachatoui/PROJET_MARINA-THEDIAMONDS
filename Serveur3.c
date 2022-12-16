b1f47316d78cd16e453930e082c9a1e37bf107c9
nachatoui <91183220+nachatoui@users.noreply.github.com>
2022-12-16 11:26:34 +0100

    Add files via upload

--- Serveur3.c
+++ Serveur3.c
@@ -90,164 +90,184 @@ int main(int argc, char* argv[])
     long num_dernier_mess_reenvoye;
     int nread;
 
+    fd_set connection;
+    int connection_server;
     pid_t child_pid;
+    struct timeval timeout_connection;
 
     while(1)
     {
-        // recvfrom est bloquant 
-        recvfrom(socket_desc, client_message, BUFFSIZE, 0,
-                (struct sockaddr*)&client_addr, &client_struct_length);
-        if(strncmp("SYN", client_message, 3) == 0)
-        {
-            sendto(socket_desc, buffer_SYN_ACK, strlen(buffer_SYN_ACK), 0,
-                (struct sockaddr*)&client_addr, client_struct_length);
-        
+        FD_ZERO(&connection);
+        FD_SET(socket_desc, &connection);
+        printf("En attente de clients.. \n");
+        timeout_connection.tv_sec = 99999;
+        timeout_connection.tv_usec = 99999;
+        connection_server = select(socket_desc + 1, &connection, NULL, NULL, &timeout_connection);
+        if(connection_server == -1){
+            perror("Erreur lors de la connexion\n");
+            exit(1);
+        }
+
+        //un client est arrivé
+        if(FD_ISSET(socket_desc, &connection) == 1){
+
+            // recvfrom est bloquant 
             recvfrom(socket_desc, client_message, BUFFSIZE, 0,
                     (struct sockaddr*)&client_addr, &client_struct_length);
-
-            if(strncmp("ACK", client_message, 3) == 0)
+            if(strncmp("SYN", client_message, 3) == 0)
             {
-                // Creation socket UDP directe avec le client:
-                Sous_socket = Creation_Socket (nvx_port, ss_addr);
-                nvx_port += 1;
-                memset(buffer_SYN_ACK, '\0', 12);     
-                sprintf(buffer_SYN_ACK, "%s%d", SYN_ACK, nvx_port);
+                sendto(socket_desc, buffer_SYN_ACK, strlen(buffer_SYN_ACK), 0,
+                    (struct sockaddr*)&client_addr, client_struct_length);
+            
+                recvfrom(socket_desc, client_message, BUFFSIZE, 0,
+                        (struct sockaddr*)&client_addr, &client_struct_length);
 
-                // Protocole connecté !
-                printf("Bien connecté ! \n");
-                printf("Client : @IP: %s et du port: %i\n",
-                    inet_ntoa(client_addr.sin_addr), ntohs(client_addr.sin_port));
+                if(strncmp("ACK", client_message, 3) == 0)
+                {
+                    // Creation socket UDP directe avec le client:
+                    Sous_socket = Creation_Socket (nvx_port, ss_addr);
+                    // nvx_port += 1;
+                    memset(buffer_SYN_ACK, '\0', 12);     
+                    sprintf(buffer_SYN_ACK, "%s%d", SYN_ACK, nvx_port);
 
-                child_pid = fork();
-                if (child_pid == -1) {
-                    perror("fork");
-                    exit(-1);
-                } else if (child_pid == 0){
-                    close(socket_desc);
-                    //Fichier demandé par le client :
-                    memset(client_message, '\0', BUFFSIZE); 
-                    recvfrom(Sous_socket, client_message, BUFFSIZE, 0,
-                                    (struct sockaddr*)&client_addr, &client_struct_length);
-                    printf("Nom du fichier demandé : %s \n", client_message);
+                    // Protocole connecté !
+                    printf("Bien connecté ! \n");
+                    printf("Client : @IP: %s et du port: %i\n",
+                        inet_ntoa(client_addr.sin_addr), ntohs(client_addr.sin_port));
 
-                    // Envoie d'un fichier:
-                    FILE *fp = fopen(client_message,"rb");
-                    if(fp == NULL) {
-                        perror ("Error in opening file");
+                    child_pid = fork();
+                    if (child_pid == -1) {
+                        perror("fork");
                         exit(-1);
-                    }
-                    fseek (fp, 0, SEEK_END);
-                    long taille_fichier = ftell(fp);
-                    fseek (fp, 0, SEEK_SET);    
-                    int last_num_seq_fichier; 
-                    if (taille_fichier % (BUFFSIZE-6) == 0)
-                    {
-                        last_num_seq_fichier = taille_fichier / (BUFFSIZE-6);
-                    }
-                    else
-                    {
-                        last_num_seq_fichier = (taille_fichier / (BUFFSIZE-6)) + 1;
-                    }
+                    } else if (child_pid == 0){
+                        close(socket_desc);
+                        //Fichier demandé par le client :
+                        memset(client_message, '\0', BUFFSIZE); 
+                        recvfrom(Sous_socket, client_message, BUFFSIZE, 0,
+                                        (struct sockaddr*)&client_addr, &client_struct_length);
+                        printf("Nom du fichier demandé : %s \n", client_message);
 
-                    // int cwnd_taille = 10; // A modifier lors optimisation - fonction du débit espéré
-                    
-
-                    fd_set rset;
-                    FD_ZERO(&rset);
-                    int nready;
-
-                    
-
-                    gettimeofday(&start,0); 
-                    while ( last_Ack_Recu != last_num_seq_fichier ) {                  
-                        if (last_Ack_Recu == num_seq) 
+                        // Envoie d'un fichier:
+                        FILE *fp = fopen(client_message,"rb");
+                        if(fp == NULL) {
+                            perror ("Error in opening file");
+                            exit(-1);
+                        }
+                        fseek (fp, 0, SEEK_END);
+                        long taille_fichier = ftell(fp);
+                        fseek (fp, 0, SEEK_SET);    
+                        int last_num_seq_fichier; 
+                        if (taille_fichier % (BUFFSIZE-6) == 0)
                         {
-                            if ( ! feof(fp) )
-                            { 
-                                for (int i=0; i<cwnd_taille; i++) 
-                                {
-                                    num_seq += 1;
-                                    envoie_message(fp, num_seq, server_message, lecture, char_num_seq, Sous_socket, client_addr);
-                                    Flight_Size += 1 ; 
-                                    printf("message envoyé n° %d !\n", num_seq);
-                                } 
-                            }
+                            last_num_seq_fichier = taille_fichier / (BUFFSIZE-6);
                         }
+                        else
+                        {
+                            last_num_seq_fichier = (taille_fichier / (BUFFSIZE-6)) + 1;
+                        }
+
+                        // int cwnd_taille = 10; // A modifier lors optimisation - fonction du débit espéré
                         
-                        // On a envoyé tous les messages possibles en fonction de la taille de notre fenêtre 
-                        RetransmissionTimeout.tv_sec = 0; // min(2,(int)(1.3*srtt)); /* checké pour trouver val optimal  */
-                        RetransmissionTimeout.tv_usec = 50;//(1.3*srtt - (int)(1.3*srtt))*1000000;
-                        /*if (srtt == 0){
-                            RetransmissionTimeout.tv_sec = 1; /*initialisation  
-                            RetransmissionTimeout.tv_usec = 0;
-                        }
-                        printf("rtt = %f, srtt = %f, RetransmissionTimeout = %ld.%06lds \n", rtt, srtt, RetransmissionTimeout.tv_sec, RetransmissionTimeout.tv_usec);*/
 
-                        FD_SET(Sous_socket, &rset);
-                        nready = select(Sous_socket+1, &rset, NULL, NULL, &RetransmissionTimeout);
-                        // On reste bloqué en attendant la fin du timeout afin de voir si le message pourra être ACK
-                        if (FD_ISSET(Sous_socket, &rset)) { 
-                            // ACK arrive avant le fin du timeout
-                            printf("ACK avant la fin du timeout \n");
-                            ACK_prededent = last_Ack_Recu; //On stocke la valeur du dernier Ack reçu
-                            last_Ack_Recu = reception_message(client_message, Sous_socket, client_addr, client_struct_length, buffer_last_Ack_Recu, ACK_prededent, &compteur_ACK_DUP);
-                            Flight_Size -= 1 ;
+                        fd_set rset;
+                        FD_ZERO(&rset);
+                        int nready;
 
-                            if (compteur_ACK_DUP == 3 ) { 
-                                if (last_Ack_Recu != (num_dernier_mess_reenvoye-1)) //suppose un même pkt pas perdu 2 fois ? 
-                                {
-                                    num_dernier_mess_reenvoye = FastRetransmit (fp, &compteur_ACK_DUP, &SlowStartSeuil, last_Ack_Recu, server_message, lecture, char_num_seq, Sous_socket, client_addr, Flight_Size);
-                                    fseek(fp, num_seq*(BUFFSIZE-6), SEEK_SET);
+                        
+
+                        gettimeofday(&start,0); 
+                        while ( last_Ack_Recu != last_num_seq_fichier ) {                  
+                            if (last_Ack_Recu == num_seq) 
+                            {
+                                if ( ! feof(fp) )
+                                { 
+                                    for (int i=0; i<cwnd_taille; i++) 
+                                    {
+                                        num_seq += 1;
+                                        envoie_message(fp, num_seq, server_message, lecture, char_num_seq, Sous_socket, client_addr);
+                                        Flight_Size += 1 ; 
+                                        printf("message envoyé n° %d !\n", num_seq);
+                                    } 
+                                }
+                            }
+                            
+                            // On a envoyé tous les messages possibles en fonction de la taille de notre fenêtre 
+                            RetransmissionTimeout.tv_sec = 0; // min(2,(int)(1.3*srtt)); /* checké pour trouver val optimal  */
+                            RetransmissionTimeout.tv_usec = 50;//(1.3*srtt - (int)(1.3*srtt))*1000000;
+                            /*if (srtt == 0){
+                                RetransmissionTimeout.tv_sec = 1; /*initialisation  
+                                RetransmissionTimeout.tv_usec = 0;
+                            }
+                            printf("rtt = %f, srtt = %f, RetransmissionTimeout = %ld.%06lds \n", rtt, srtt, RetransmissionTimeout.tv_sec, RetransmissionTimeout.tv_usec);*/
+
+                            FD_SET(Sous_socket, &rset);
+                            nready = select(Sous_socket+1, &rset, NULL, NULL, &RetransmissionTimeout);
+                            // On reste bloqué en attendant la fin du timeout afin de voir si le message pourra être ACK
+                            if (FD_ISSET(Sous_socket, &rset)) { 
+                                // ACK arrive avant le fin du timeout
+                                printf("ACK avant la fin du timeout \n");
+                                ACK_prededent = last_Ack_Recu; //On stocke la valeur du dernier Ack reçu
+                                last_Ack_Recu = reception_message(client_message, Sous_socket, client_addr, client_struct_length, buffer_last_Ack_Recu, ACK_prededent, &compteur_ACK_DUP);
+                                Flight_Size -= 1 ;
+
+                                if (compteur_ACK_DUP == 3 ) { 
+                                    if (last_Ack_Recu != (num_dernier_mess_reenvoye-1)) //suppose un même pkt pas perdu 2 fois ? 
+                                    {
+                                        num_dernier_mess_reenvoye = FastRetransmit (fp, &compteur_ACK_DUP, &SlowStartSeuil, last_Ack_Recu, server_message, lecture, char_num_seq, Sous_socket, client_addr, Flight_Size);
+                                        fseek(fp, num_seq*(BUFFSIZE-6), SEEK_SET);
+                                    } else {
+                                        compteur_ACK_DUP = 0;
+                                        if (cwnd_taille < SlowStartSeuil)
+                                        {
+                                            //slow start
+                                            cwnd_taille +=1;
+                                        
+                                        }  else {
+                                            //congestion avoidance
+                                            cwnd_taille = cwnd_taille + 1/cwnd_taille;
+                                        }
+                                    }
                                 } else {
-                                    compteur_ACK_DUP = 0;
                                     if (cwnd_taille < SlowStartSeuil)
                                     {
                                         //slow start
                                         cwnd_taille +=1;
-                                    
                                     }  else {
                                         //congestion avoidance
-                                        cwnd_taille = cwnd_taille + 1/cwnd_taille;
+                                        cwnd_taille = cwnd_taille + 1/max(1,cwnd_taille);
                                     }
                                 }
                             } else {
-                                if (cwnd_taille < SlowStartSeuil)
-                                {
-                                    //slow start
-                                    cwnd_taille +=1;
-                                }  else {
-                                    //congestion avoidance
-                                    cwnd_taille = cwnd_taille + 1/max(1,cwnd_taille);
-                                }
-                            }
-                        } else {
-                            // Paquets perdus après un timeout => Retransmission 
-                            printf("Fin du timeout= %ld.%06lds, paquet perdu \n", RetransmissionTimeout.tv_sec, RetransmissionTimeout.tv_usec);
-                            envoie_message(fp, last_Ack_Recu+1, server_message, lecture, char_num_seq, Sous_socket, client_addr);
-                            printf("message réenvoyé n° %ld !\n", last_Ack_Recu+1);
-                            SlowStartSeuil = Flight_Size / 2;
-                            cwnd_taille = 1;
-                            fseek(fp, num_seq*(BUFFSIZE-6), SEEK_SET);
-                        } 
+                                // Paquets perdus après un timeout => Retransmission 
+                                printf("Fin du timeout= %ld.%06lds, paquet perdu \n", RetransmissionTimeout.tv_sec, RetransmissionTimeout.tv_usec);
+                                envoie_message(fp, last_Ack_Recu+1, server_message, lecture, char_num_seq, Sous_socket, client_addr);
+                                printf("message réenvoyé n° %ld !\n", last_Ack_Recu+1);
+                                SlowStartSeuil = Flight_Size / 2;
+                                cwnd_taille = 1;
+                                fseek(fp, num_seq*(BUFFSIZE-6), SEEK_SET);
+                            } 
+                        }
+                        fclose(fp);
+                        sendto(Sous_socket, FIN, strlen(FIN), 0,
+                                (struct sockaddr*)&client_addr, client_struct_length);
+                        close(Sous_socket);
+                        gettimeofday(&end,0); 
+                        temps_exec = differencetemps(start, end);
+                        printf("taille_fichier %ld \n", taille_fichier);
+                        debit = (taille_fichier/temps_exec)*0.000001;
+                        printf("Realise en %f s, debit %f Mo/s \n", temps_exec, debit);
+                        exit(-1);
+                    } else {
+                        close(Sous_socket);
+                        nvx_port = nvx_port + 1;
                     }
-                    fclose(fp);
-                    sendto(Sous_socket, FIN, strlen(FIN), 0,
-                            (struct sockaddr*)&client_addr, client_struct_length);
-                    close(Sous_socket);
-                    gettimeofday(&end,0); 
-                    temps_exec = differencetemps(start, end);
-                    printf("taille_fichier %ld \n", taille_fichier);
-                    debit = (taille_fichier/temps_exec)*0.000001;
-                    printf("Realise en %f s, debit %f Mo/s \n", temps_exec, debit);
-                    exit(-1);
-                } else {
-                    close(Sous_socket);
                 }
             } else {
                 printf("erreur Threeway handshake\n");
             }
         }
         printf("Fin client\n");
+        timeout_connection.tv_sec = 99999;
     }
     close(socket_desc);
     return 0;
